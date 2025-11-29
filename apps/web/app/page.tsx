7f5f29456f5bdcbcad6fb4f89d8cce5a47c61598
farcdigger <alivelicrypto@gmail.com>
2025-11-30 02:51:51 +0300

    fix: Update website URL to xfroranft.xyz and add external link icon
    - Change website link to https://xfroranft.xyz/
    - Add external link icon to indicate it opens in new tab
    - Keep globe icon for website identification

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1233,6 +1233,9 @@ function HomePageContent() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                       </svg>
                       <span className="font-semibold">Visit Website</span>
+                      <svg className="w-4 h-4 ml-auto opacity-50" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                      </svg>
                     </a>
                     
                     <a
